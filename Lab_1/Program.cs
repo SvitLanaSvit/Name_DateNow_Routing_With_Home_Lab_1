30d56fb3db5dc893834b8c93ab2d2ec585bdd458
Spil_jf00@student.itstep.org <Spil_jf00@student.itstep.org>
2023-01-17 18:51:46 +0100

    Добавьте файлы проекта.

---
+++ Lab_1/Program.cs
@@ -0,0 +1,36 @@
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Kizilpinar Svitlana");
+app.MapGet("home/info", (context) =>
+{
+    StringBuilder b = new StringBuilder();
+    if (context.Request.Query.ContainsKey("firstname"))
+    {
+        string firstname = context.Request.Query["firstname"];
+        b.AppendLine($"<div>Firstname: {firstname}</div>");
+    }
+    if (context.Request.Query.ContainsKey("lastname"))
+    {
+        string lastname = context.Request.Query["lastname"];
+        b.AppendLine($"<div>Lastname: {lastname}</div>");
+    }
+    return context.Response.WriteAsync(b.ToString());
+});
+app.Map("home/date", (context) =>
+{
+    string date = DateTime.Now.ToString("dd/MM/yyyy");
+    return context.Response.WriteAsync(date);   
+});
+app.Map("home/list", (context) =>
+{
+    StringBuilder sb = new StringBuilder();
+    sb.AppendLine($"<ul><li>Coffee</li><li>Tee</li><li>Milch</li></ul>");
+    context.Response.Headers.Add("Content-Type", "text/html;charset=utf-8");
+    return context.Response.WriteAsync(sb.ToString());
+});
+app.Map("home/", () => "Start");
+
+app.Run();
